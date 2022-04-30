867a9fb98f0d34213c62c075cfd3212d1a032a66
iljadedeiko <lafisrampage123@gmail.com>
2022-04-30 20:09:44 +0300

    Query all movies

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', 'app_movies')]
     public function index(): Response
     {
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->getClassName();
+        dd($movies);
+
         return $this->render('layouts/index.html.twig');
     }
 }
